c1f201137206b99976047ebbe3309f4bc7bab17f
Nj <jumrsup@hi2.in>
2021-03-13 09:45:18 +0300

    feature: add code for calculation values

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || min(undefined)|| array.length == 0) return 0;
+  return (Math.min(...array))
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || max(undefined)|| array.length == 0) return 0;
+  return (Math.max(...array))
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || avg(undefined)|| array.length == 0) return 0;
+  let k = array.reduce(function(a,b){ return a+b},0) / array.length;
+  return k
 }
